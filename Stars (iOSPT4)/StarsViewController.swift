d9eb1b4310b1982db848abad8b5fcbba27c291e5
David <davidtwright89@gmail.com>
2019-12-05 22:09:44 -0500

    set up views and outlets

---
+++ Stars (iOSPT4)/StarsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Stars (iOSPT4)
+//
+//  Created by David Wright on 12/5/19.
+//  Copyright © 2019 David Wright. All rights reserved.
+//
+
+import UIKit
+
+class StarsViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var distanceTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - IBActions
+    @IBAction func printStars(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createStar(_ sender: UIButton) {
+        
+    }
+    
+}
+
